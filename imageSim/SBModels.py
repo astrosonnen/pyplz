10e577693d3d31864ee234023e37187a4f3747e5
Alessandro Sonnenfeld <sonnen@physics.ucsb.edu>
2018-03-27 21:08:09 +0900

    removed obsolete function

--- imageSim/SBModels.py
+++ imageSim/SBModels.py
@@ -6,54 +6,11 @@ import os
 
 
 tempdir = os.environ.get('PYLENSDIR') + 'pylens/templates/'
-sersicpars = ['x', 'y', 'q', 're', 'pa', 'n']
+parlists = {'Sersic': ['x', 'y', 'q', 're', 'pa', 'n'], 'PSF': ['x', 'y']}
 
 def cnts2mag(cnts,zp):
     from math import log10
     return -2.5*log10(cnts) + zp
-
-
-def etau_madau(wl, z):
-    """
-    Madau 1995 extinction for a galaxy spectrum at redshift z 
-    defined on a wavelength grid wl
-    """
-    n=len(wl)
-    l=np.array([1216.,1026.,973.,950.])
-    xe=1.+z
-    
-    #If all the spectrum is redder than (1+z)*wl_lyman_alfa 
-    if wl[0]> l[0]*xe: return np.zeros(n)+1.
-    
-    #Madau coefficients
-    c=np.array([3.6e-3,1.7e-3,1.2e-3,9.3e-4])
-    ll=912.
-    tau=wl*0.
-    i1=np.searchsorted(wl,ll)
-    i2=n-1
-    #Lyman series absorption
-    for i in range(len(l)):
-	i2=np.searchsorted(wl[i1:i2],l[i]*xe)
-	tau[i1:i2]=tau[i1:i2]+c[i]*(wl[i1:i2]/l[i])**3.46
-
-    if ll*xe < wl[0]:
-        return np.exp(-tau)
-
-    #Photoelectric absorption
-    xe=1.+z
-    i2=np.searchsorted(wl,ll*xe)
-    xc=wl[i1:i2]/ll
-    xc3=xc**3
-    tau[i1:i2]=tau[i1:i2]+\
-                (0.25*xc3*(xe**.46-xc**0.46)\
-                 +9.4*xc**1.5*(xe**0.18-xc**0.18)\
-                 -0.7*xc3*(xc**(-1.32)-xe**(-1.32))\
-                 -0.023*(xe**1.68-xc**1.68))
-
-    tau = np.clip(tau, 0, 700)
-    return np.exp(-tau)
-    # if tau>700. : return 0.
-    # else: return exp(-tau)
 
 
 class SBModel:
