0990c55e32bbaca2cb787fb1f528c57e8077b6e8
Alessandro Sonnenfeld <alessandro.sonnenfeld@ipmu.jp>
2018-04-25 15:57:12 +0900

    added functions for stellar mass fit

---
+++ pyplz_mstar_fitters.py
@@ -0,0 +1,114 @@
+import emcee
+import pymc
+import numpy as np
+import h5py
+from scipy.stats import truncnorm
+from scipy.optimize import basinhopping
+from scipy.interpolate import splrep, splev, splint
+import luminosity_functions
+import pyplz_cosmology
+from pyplz_cosmology import omegaL, omegaM
+
+
+def run_mcmc(model, chainname, nwalkers=100, nsteps=1000):
+
+    start = []
+    npars = len(model.pars)
+
+    for j in range(npars):
+
+        a, b = (model.pars[j].lower - model.pars[j].value)/model.pars[j].step, (model.pars[j].upper - model.pars[j].value)/model.pars[j].step
+        tmp = truncnorm.rvs(a, b, size=nwalkers)*model.pars[j].step + model.pars[j].value
+        start.append(tmp)
+
+    start = np.array(start).T
+
+    def logprior(allpars):
+        for i in range(npars):
+            if allpars[i] < model.pars[i].lower or allpars[i] > model.pars[i].upper:
+                return -np.inf
+        return 0.
+
+    fakemags = []
+
+    for i in range(model.nlight):
+        magdic = {}
+        for band in model.bands:
+            magdic[band] = 99.
+        fakemags.append(magdic)
+
+    for i in range(model.nsource):
+        magdic = {}
+        for band in model.bands:
+            magdic[band] = 99.
+        fakemags.append(magdic)
+
+    def logpfunc(allpars):
+        lp = logprior(allpars)
+        if not np.isfinite(lp):
+            return -np.inf, fakemags
+
+        for j in range(npars):
+            model.pars[j].value = allpars[j]
+
+        model.update()
+
+        chi2 = model.get_chi2()
+        logp = -0.5*chi2
+
+        if logp != logp:
+            return -np.inf, fakemags
+
+        allmags = []
+
+        for i in range(model.nlight):
+            magdic = {}
+            for band in model.bands:
+                magdic[band] = model.light_sed_models[i].mags[band]
+            allmags.append(magdic)
+    
+        for i in range(model.nsource):
+            magdic = {}
+            for band in model.bands:
+                magdic[band] = model.source_sed_models[i].mags[band]
+            allmags.append(magdic)
+     
+        return logp, allmags
+
+    sampler = emcee.EnsembleSampler(nwalkers, npars, logpfunc)
+
+    print "fitting model..."
+
+    sampler.run_mcmc(start, nsteps)
+
+    chain = sampler.chain
+    magschain = sampler.blobs
+
+    outchain = {}
+    outchain['logp'] = sampler.lnprobability
+
+    for i in range(npars):
+        outchain[model.index2par[i]] = chain[:, :, i]
+
+    for i in range(model.nlight):
+        for band in model.bands:
+            outchain['light%d.mag_%s'%(i+1, band)] = np.zeros((nwalkers, nsteps))
+
+    for i in range(model.nsource):
+        for band in model.bands:
+            outchain['source%d.mag_%s'%(i+1, band)] = np.zeros((nwalkers, nsteps))
+
+    for i in range(nsteps):
+        for j in range(nwalkers):
+            for band in model.bands:
+                for l in range(model.nlight):
+                    outchain['light%d.mag_%s'%(l+1, band)][j, i] = magschain[i][j][l][band]
+                for s in range(model.nsource):
+                    outchain['source%d.mag_%s'%(s+1, band)][j, i] = magschain[i][j][model.nlight+s][band]
+
+    h5py_file = h5py.File(chainname, 'w')
+    for par in outchain:
+        h5py_file.create_dataset(par, data=outchain[par])
+
+
+
---
+++ run_pyplz_mstar.py
@@ -0,0 +1,98 @@
+import numpy as np
+import sys
+import h5py
+import os
+import pyplz_mstar_objects, pyplz_mstar_fitters
+
+
+inputfound = False
+nargv = len(sys.argv)
+i = 1
+key = None
+saveind = None
+catlist = False
+action = None
+while i <= nargv and not inputfound:
+    argv = sys.argv[i]
+    if argv[0] == '-':
+        key = argv[1:]
+        if key == 'c':
+            saveind = int(sys.argv[i+1])
+            inputfile = sys.argv[i+2]
+            inputfound = True
+        elif key == 's':
+            inputfile = sys.argv[i+1]
+            inputfound = True
+        elif key == 'l':
+            catlist = True
+            inputfile = sys.argv[i+1]
+            inputfound = True
+        elif key == 'M':
+            inputfile = sys.argv[i+1]
+            inputfound = True
+        else:
+            print 'unknown option -%s'%key
+            df
+    else:
+        print 'must specify mode: -s, -l, -M, -c'
+        df
+    i += 1
+
+confnames = []
+if catlist:
+    f = open(inputfile, 'r')
+    lines = f.readlines()
+    f.close()
+
+    for line in lines:
+        confnames.append(line.rstrip())
+else:
+    confnames = [inputfile]
+
+for configfile in confnames:
+
+    print configfile
+
+    config = pyplz_mstar_objects.read_config(configfile)
+    model = pyplz_mstar_objects.PyPLZModel(config)
+
+    if key == 'M' or key == 'l':
+   
+        chainname = config['output_dir']+configfile+'_chain.hdf5'
+        for par in model.pars:
+            print par.name
+        pyplz_mstar_fitters.run_mcmc(model, chainname, config['Nwalkers'], config['Nsteps'])
+
+        chain = h5py.File(chainname, 'r')
+
+        ML = chain['logp'].value.argmax()
+        modelname = config['output_dir']+configfile+'_ML'
+        for i in range(len(model.pars)):
+            model.pars[i].value = chain['%s'%model.index2par[i]].value.flatten()[ML]
+
+        model.update()
+
+        model.save(modelname)
+        model.write_config_file(config, modelname)
+
+    elif key == 'c':
+    
+        chain = h5py.File(config['output_dir']+configfile+'_chain.hdf5', 'r')
+
+        modelname = config['output_dir']+configfile+'_%06d'%saveind
+        for i in range(len(model.pars)):
+            model.pars[i].value = chain['%s'%model.index2par[i]].value.flatten()[saveind]
+
+        model.update()
+
+        model.write_config_file(config, modelname)
+  
+    elif key == 's':
+
+        modelname = config['output_dir']+configfile
+
+        model.update()
+
+        model.save(modelname)
+
+
