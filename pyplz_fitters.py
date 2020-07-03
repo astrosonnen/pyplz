dd928ca6e73b08c44dfd9a037be6124172fe5485
Alessandro Sonnenfeld <sonnenfeld@strw.leidenuniv.nl>
2020-07-03 12:23:18 +0200

    fixed bug related to single band fit

--- pyplz_fitters.py
+++ pyplz_fitters.py
@@ -87,7 +87,7 @@ def run_mcmc(model, chainname, walkers=100, nsteps=1000):
     sampler.run_mcmc(start, nsteps)
 
     chain = sampler.chain
-    magschain = sampler.blobs
+    magschain = sampler.blobs.reshape((nsteps, nwalkers, nbands))
 
     outchain = {}
     outchain['logp'] = sampler.lnprobability
